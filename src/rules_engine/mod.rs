c049a39eeda0aafc9c2ae4bedcf04d977b899ef3
Nathan Douglas <github@darkdell.net>
2024-03-17 16:39:42 -0400

    Preludination.

--- src/rules_engine/mod.rs
+++ src/rules_engine/mod.rs
@@ -5,9 +5,9 @@ pub mod traits;
 pub mod values;
 
 pub mod prelude {
-  // pub use super::conclusions::prelude::*;
-  // pub use super::conditions::prelude::*;
-  // pub use super::operations::prelude::*;
+  pub use super::conclusions::prelude::*;
+  pub use super::conditions::prelude::*;
+  pub use super::operations::prelude::*;
   pub use super::traits::prelude::*;
   pub use super::values::prelude::*;
 }
