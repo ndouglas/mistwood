87039894def8603306f71e7d0b8de82fe4cd781b
Nathan Douglas <github@darkdell.net>
2024-03-17 15:18:41 -0400

    Argument -> Value pt 2

--- src/rules_engine/mod.rs
+++ src/rules_engine/mod.rs
@@ -1,13 +1,13 @@
-pub mod arguments;
 pub mod conclusions;
 pub mod conditions;
 pub mod operations;
 pub mod traits;
+pub mod values;
 
 pub mod prelude {
-  pub use super::arguments::prelude::*;
   // pub use super::conclusions::prelude::*;
   // pub use super::conditions::prelude::*;
   // pub use super::operations::prelude::*;
   pub use super::traits::prelude::*;
+  pub use super::values::prelude::*;
 }
--- src/rules_engine/traits/mod.rs
+++ src/rules_engine/traits/mod.rs
@@ -1,10 +1,10 @@
-pub mod argument;
 pub mod conclusion;
 pub mod condition;
 pub mod context;
 pub mod operation;
+pub mod value;
 
 pub mod prelude {
-  pub use super::argument::prelude::*;
   pub use super::operation::prelude::*;
+  pub use super::value::prelude::*;
 }
