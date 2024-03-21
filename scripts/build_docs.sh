62194401640e70be159f48dc982f77aae6563bd0
Nathan Douglas <github@darkdell.net>
2024-03-21 08:11:50 -0400

    Refactor some workflows and hopefully fix documentation and make it testable in CI.

--- scripts/build_docs.sh
+++ scripts/build_docs.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+function die() {
+  >&2 echo "$1";
+  exit 1;
+}
+
 # This script is used to build the documentation for the project.
 #
 # There are currently two sources of documentation:
@@ -8,22 +13,12 @@
 
 base_dir="$(git rev-parse --show-toplevel)";
 pushd "${base_dir}" > /dev/null; # cwd -> base_dir
-cargo +nightly doc \
+cargo doc \
   --all-features \
   --no-deps \
   --document-private-items \
-  --target-dir docs/src/rustdoc;
+  --target-dir docs/src/rustdoc || die "Failed to build rustdoc";
 pushd docs > /dev/null; # base_dir -> base_dir/docs
-mdbook build .;
+mdbook build . || die "Failed to build mdbook";
 popd > /dev/null; # base_dir/docs -> base_dir
 popd > /dev/null; # base_dir -> cwd
-
-# cd $BASE_DIR
-# cargo +nightly doc --all-features
-# (cd guide
-#     (cd src && sed -e "s#{ROOT_PATH}#${1-/}#g" links.md.template > links.md)
-#     mdbook build
-#     mkdir -p book/{assets,rustdoc}
-#     cp -r assets/* book/assets/
-#     cp -r ../target/doc/* book/rustdoc/
-# )
