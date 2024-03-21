16128c5953d04c5262f4e35a89beb1532f15aac2
Nathan Douglas <github@darkdell.net>
2024-03-21 07:32:06 -0400

    Completes service container :tada:

--- src/di/_traits/get_input.rs
+++ src/di/_traits/get_input.rs
@@ -23,3 +23,42 @@ impl<S: GetInput, T: GetInput> GetInput for (S, T) {
     S::get_input(provider).and_then(|s| T::get_input(provider).map(|t| (s, t)))
   }
 }
+
+macro_rules! impl_get_input_for_tuple {
+  ( $($name:ident),* ) => {
+    /// Implement `GetInput` for a tuple of types.
+    impl<$($name: GetInput),*> GetInput for ($($name,)*) {
+      /// Get the input for a type `T` from the given `InputProvider`.
+      fn get_input<IP: InputProvider>(provider: &IP) -> Option<Self> {
+        Some(($($name::get_input(provider)?,)*))
+      }
+    }
+  };
+}
+
+impl_get_input_for_tuple!(A);
+// impl_get_input_for_tuple!(A, B);
+impl_get_input_for_tuple!(A, B, C);
+impl_get_input_for_tuple!(A, B, C, D);
+impl_get_input_for_tuple!(A, B, C, D, E);
+impl_get_input_for_tuple!(A, B, C, D, E, F);
+impl_get_input_for_tuple!(A, B, C, D, E, F, G);
+impl_get_input_for_tuple!(A, B, C, D, E, F, G, H);
+impl_get_input_for_tuple!(A, B, C, D, E, F, G, H, I);
+impl_get_input_for_tuple!(A, B, C, D, E, F, G, H, I, J);
+impl_get_input_for_tuple!(A, B, C, D, E, F, G, H, I, J, K);
+impl_get_input_for_tuple!(A, B, C, D, E, F, G, H, I, J, K, L);
+impl_get_input_for_tuple!(A, B, C, D, E, F, G, H, I, J, K, L, M);
+impl_get_input_for_tuple!(A, B, C, D, E, F, G, H, I, J, K, L, M, N);
+impl_get_input_for_tuple!(A, B, C, D, E, F, G, H, I, J, K, L, M, N, O);
+impl_get_input_for_tuple!(A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P);
+impl_get_input_for_tuple!(A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q);
+impl_get_input_for_tuple!(A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R);
+impl_get_input_for_tuple!(A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S);
+impl_get_input_for_tuple!(A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T);
+impl_get_input_for_tuple!(A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U);
+impl_get_input_for_tuple!(A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V);
+impl_get_input_for_tuple!(A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W);
+impl_get_input_for_tuple!(A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X);
+impl_get_input_for_tuple!(A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y);
+impl_get_input_for_tuple!(A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z);
--- src/di/container.rs
+++ src/di/container.rs
@@ -64,3 +64,66 @@ impl InputProvider for Container {
     self.0.get::<Object<T>>().cloned()
   }
 }
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[derive(Debug, PartialEq)]
+  struct NewString(String);
+
+  impl Builder for NewString {
+    type Input = ();
+    type Output = NewString;
+    fn build(_: Self::Input) -> Self::Output {
+      NewString("Hello, world!".to_string())
+    }
+  }
+
+  #[derive(Debug)]
+  struct NewInt(i32);
+
+  impl Builder for NewInt {
+    type Input = ();
+    type Output = NewInt;
+    fn build(_: Self::Input) -> Self::Output {
+      NewInt(42)
+    }
+  }
+
+  #[derive(Debug)]
+  struct FactProvider {
+    new_string: Object<NewString>,
+    new_int: Object<NewInt>,
+  }
+
+  impl Builder for FactProvider {
+    type Input = (Object<NewString>, Object<NewInt>);
+    type Output = FactProvider;
+    fn build((new_string, new_int): Self::Input) -> Self::Output {
+      FactProvider { new_string, new_int }
+    }
+  }
+
+  #[test]
+  fn test_container() {
+    let mut container = Container::new();
+    container.build::<NewString>();
+    let string = container.get::<NewString>().unwrap();
+    assert_eq!(string.lock().unwrap().0, "Hello, world!".to_string());
+  }
+
+  #[test]
+  fn test_fact_provider() {
+    let mut container = Container::new();
+    container.build::<NewString>();
+    container.build::<NewInt>();
+    container.build::<FactProvider>();
+    let fact_provider = container.get::<FactProvider>().unwrap();
+    assert_eq!(
+      fact_provider.lock().unwrap().new_string.lock().unwrap().0,
+      "Hello, world!".to_string()
+    );
+    assert_eq!(fact_provider.lock().unwrap().new_int.lock().unwrap().0, 42);
+  }
+}
