72712cf581a0f511dd9595774cfeb555a0af5e9c
Nathan Douglas <github@darkdell.net>
2024-03-20 20:09:42 -0400

    Adds TypeMap type.

---
+++ src/_types.rs
@@ -0,0 +1,5 @@
+pub mod type_map;
+
+pub mod prelude {
+  pub use super::type_map::*;
+}
---
+++ src/_types/type_map.rs
@@ -0,0 +1,59 @@
+use std::any::{Any, TypeId};
+use std::collections::HashMap;
+
+/// A map from types to values.
+///
+/// This is a simple wrapper around `HashMap<TypeId, Box<dyn Any>>` that allows
+/// for type-safe access to the stored values.
+///
+/// This enables type-safe registries, event handlers, dependency injection,
+/// and other use cases where you want to store and retrieve values by type.
+///
+/// @see <https://willcrichton.net/rust-api-type-patterns/registries.html>
+#[derive(Default)]
+pub struct TypeMap(HashMap<TypeId, Box<dyn Any>>);
+
+impl TypeMap {
+  /// Create a new, empty `TypeMap`.
+  pub fn new() -> Self {
+    Self(HashMap::new())
+  }
+
+  /// Set a value of type `T`.
+  pub fn set<T: Any + 'static>(&mut self, t: T) {
+    self.0.insert(TypeId::of::<T>(), Box::new(t));
+  }
+
+  /// Check if a value of type `T` is present.
+  pub fn has<T: Any + 'static>(&self) -> bool {
+    self.0.contains_key(&TypeId::of::<T>())
+  }
+
+  /// Get a reference to a value of type `T`.
+  pub fn get<T: Any + 'static>(&self) -> Option<&T> {
+    self.0.get(&TypeId::of::<T>()).map(|t| t.downcast_ref::<T>().unwrap())
+  }
+
+  /// Get a mutable reference to a value of type `T`.
+  pub fn get_mut<T: Any + 'static>(&mut self) -> Option<&mut T> {
+    self
+      .0
+      .get_mut(&TypeId::of::<T>())
+      .map(|t| t.downcast_mut::<T>().unwrap())
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn test_type_map() {
+    let mut map = TypeMap::new();
+    assert_eq!(map.has::<i32>(), false);
+    map.set(42);
+    assert_eq!(map.has::<i32>(), true);
+    assert_eq!(map.get::<i32>(), Some(&42));
+    assert_eq!(map.get_mut::<i32>(), Some(&mut 42));
+  }
+}
