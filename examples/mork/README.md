01903e8fb15c9390ed8a7858c3bd805bc3e31503
Nathan Douglas <github@darkdell.net>
2024-03-23 16:24:48 -0400

    Adds Mork example. Closes #74.

---
+++ examples/mork/README.md
@@ -0,0 +1,16 @@
+# _Mork_
+
+Mork is a clone of Zork using the Mistwood library.
+
+░▒▓██████████████▓▒░ ░▒▓██████▓▒░░▒▓███████▓▒░░▒▓█▓▒░░▒▓█▓▒░
+░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░
+░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░
+░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓███████▓▒░░▒▓███████▓▒░
+░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░
+░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░
+░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓██████▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░
+
+The goal is to reimplement a classic text adventure game, building out the necessary functionality in the Mistwood library as we go.
+
+For reference purposes, I have a copy of the source code of _Zork_ as released [here](https://github.com/historicalsource/zork1). It is not released under an open source license. I do not intend to violate copyright by redistributing it, and I should be clear that I'll likely modify it extensively (in the form of adding comments, etc) as I work on this project.
+
---
+++ examples/mork/main.rs
@@ -0,0 +1,31 @@
+//! Mork is a clone of Zork using the Mistwood library.
+//!
+//! ░▒▓██████████████▓▒░ ░▒▓██████▓▒░░▒▓███████▓▒░░▒▓█▓▒░░▒▓█▓▒░
+//! ░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░
+//! ░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░
+//! ░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓███████▓▒░░▒▓███████▓▒░
+//! ░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░
+//! ░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░
+//! ░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓██████▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░
+//!
+//! The goal is to reimplement a classic text adventure game, building out the
+//! necessary functionality in the Mistwood library as we go.
+#[macro_use]
+extern crate mistwood;
+use mistwood::messaging::_traits::template_processor::TemplateProcessor as TemplateProcessorTrait;
+use mistwood::messaging::prelude::TemplateProcessor;
+use mistwood::messaging::prelude::TemplateProvider;
+use mistwood::messaging::stock_templates::goodbye::Goodbye;
+use mistwood::messaging::stock_templates::thanks_for_playing::ThanksForPlaying;
+
+fn main() {
+  let mut processor = TemplateProcessor::new();
+  let mut provider = TemplateProvider::new();
+  processor.set_strict_mode(true);
+  let thanks_for_playing = info_message!(provider, ThanksForPlaying, {
+    game: "Mistwood".to_string(),
+  });
+  let goodbye = info_message!(provider, Goodbye);
+  println!("{}", processor.process_message(&thanks_for_playing).unwrap());
+  println!("{}", processor.process_message(&goodbye).unwrap());
+}
