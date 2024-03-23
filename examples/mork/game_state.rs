8a34b0891d1897a3afd655cc3c1f117241127dc6
Nathan Douglas <github@darkdell.net>
2024-03-23 17:29:08 -0400

    Very, very early stages on Mork.

---
+++ examples/mork/game_state.rs
@@ -0,0 +1,18 @@
+pub struct GameState {}
+
+impl GameState {
+  /// Create a new game state.
+  pub fn new() -> Self {
+    GameState {}
+  }
+
+  /// Describe the current location.
+  pub fn describe_current_location(&self) -> String {
+    "You are in a dark, damp cave.".to_string()
+  }
+
+  /// Process the player's input.
+  pub fn process_input(&mut self, input: &str) -> Result<String, &'static str> {
+    Ok("You move forward.".to_string())
+  }
+}
