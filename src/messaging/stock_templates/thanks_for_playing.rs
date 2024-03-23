e6e3842024300ae6dbcd03db8b713a9a589a808b
Nathan Douglas <github@darkdell.net>
2024-03-23 09:07:43 -0400

    Swap out messages.

---
+++ src/messaging/stock_templates/thanks_for_playing.rs
@@ -0,0 +1,11 @@
+template_list!(GameThanks, [
+  "Thank you for playing {{ game }}!",
+  "We hope you enjoyed your adventure in {{ game }}. Thank you for playing!",
+  "Your journey in {{ game }} is just beginning. Thanks for playing!",
+  "Thank you for exploring {{ game }} with us! Where will you go next?",
+  "{{ game }} wouldn't be the same without you. Thank you for being part of this adventure!",
+  "{{ game }} is vast and full of secrets. Thank you for exploring, and return soon!",
+  "The paths you've walked are just the beginning. What will you discover next?",
+], {
+  game: String,
+});
