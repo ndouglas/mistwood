984a985a5a09872e266f7bcb1475ceaecd56c649
Nathan Douglas <github@darkdell.net>
2024-03-22 22:32:00 -0400

    PRNG Registry. Closes #60.

--- src/prng.rs
+++ src/prng.rs
@@ -1,7 +1,13 @@
+/// Types for pseudorandom number generators.
+pub mod _types;
 /// A pseudorandom number generator factory.
 pub mod factory;
+/// A registry of pseudorandom number generators.
+pub mod registry;
 
 /// Prelude.
 pub mod prelude {
+  pub use super::_types::SafePrng;
   pub use super::factory::Factory;
+  pub use super::registry::Registry;
 }
