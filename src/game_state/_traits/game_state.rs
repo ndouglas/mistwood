6d77f54496448ff841ea411c41fa1fe0ce322005
Nathan Douglas <github@darkdell.net>
2024-03-24 18:07:15 -0400

    Configures quit flag on game state, and queries it in game loop.

--- src/game_state/_traits/game_state.rs
+++ src/game_state/_traits/game_state.rs
@@ -1,2 +1,7 @@
 /// A trait for the game state.
-pub trait GameState {}
+pub trait GameState {
+  /// Determine if the game loop should exit.
+  fn get_quit_flag(&self) -> bool;
+  /// Set the game loop to exit.
+  fn set_quit_flag(&mut self, quit_flag: bool);
+}
