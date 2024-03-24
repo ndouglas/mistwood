2c5a55eb6dc428208c29196be025c1554937a7b5
Nathan Douglas <github@darkdell.net>
2024-03-24 13:10:00 -0400

    Test game loop.

--- src/game_loop/_traits/game_loop.rs
+++ src/game_loop/_traits/game_loop.rs
@@ -9,13 +9,17 @@ pub trait GameLoop {
     self.setup();
     // The inner core of the game loop.
     // Until we have been told to quit...
-    while !self.is_finished() {
+    loop {
       // Handle player commands or AI decisions.
       self.process_input();
       // Update game state, NPC behaviors, environment changes, etc.
       self.update();
       // Send updates to players or render the game state in some form.
       self.render();
+      // Check if we should exit the game loop.
+      if self.is_finished() {
+        break;
+      }
     }
     // Perform any necessary cleanup before the game loop exits.
     self.teardown();
@@ -32,4 +36,40 @@ pub trait GameLoop {
   fn render(&self);
   /// Perform any necessary cleanup before the game loop exits.
   fn teardown(&self);
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  struct TestGameLoop {
+    finished: bool,
+  }
+
+  impl GameLoop for TestGameLoop {
+    fn setup(&self) {
+      println!("Setting up game loop...");
+    }
+    fn is_finished(&self) -> bool {
+      self.finished
+    }
+    fn process_input(&self) {
+      println!("Processing input...");
+    }
+    fn update(&self) {
+      println!("Updating game state...");
+    }
+    fn render(&self) {
+      println!("Rendering game state...");
+    }
+    fn teardown(&self) {
+      println!("Tearing down game loop...");
+    }
+  }
+
+  #[test]
+  fn test_game_loop() {
+    let game_loop = TestGameLoop { finished: true };
+    game_loop.run();
+  }
 }
