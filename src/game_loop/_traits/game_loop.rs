9007f67bbf9042ffd3bf6fce31e3cf114f97d38c
Nathan Douglas <github@darkdell.net>
2024-03-24 18:07:15 -0400

    Configures quit flag on game state, and queries it in game loop.

--- src/game_loop/_traits/game_loop.rs
+++ src/game_loop/_traits/game_loop.rs
@@ -1,75 +1,85 @@
+use crate::game_state::_traits::game_state::GameState;
+
 /// A trait defining a game loop.
 ///
 /// This trait defines a template for a synchronous game loop, the thought
 /// being that a synchronous one is easier to define, develop, and debug.
 pub trait GameLoop {
   /// The actual game loop.
-  fn run(&self) {
+  fn run(&self, state: &mut dyn GameState) {
     // Initialize game world, load assets, etc.
-    self.setup();
+    self.setup(state);
     // The inner core of the game loop.
     // Until we have been told to quit...
-    loop {
+    while !self.is_finished(state) {
       // Handle player commands or AI decisions.
-      self.process_input();
+      self.process_input(state);
       // Update game state, NPC behaviors, environment changes, etc.
-      self.update();
+      self.update(state);
       // Send updates to players or render the game state in some form.
-      self.render();
-      // Check if we should exit the game loop.
-      if self.is_finished() {
-        break;
-      }
+      self.render(state);
     }
     // Perform any necessary cleanup before the game loop exits.
-    self.teardown();
+    self.teardown(state);
   }
   /// Initialize game world, load assets, etc.
-  fn setup(&self);
+  fn setup(&self, state: &mut dyn GameState);
   /// Determine if the game loop should exit.
-  fn is_finished(&self) -> bool;
+  fn is_finished(&self, state: &mut dyn GameState) -> bool {
+    state.get_quit_flag()
+  }
   /// Handle player commands or AI decisions.
-  fn process_input(&self);
+  fn process_input(&self, state: &mut dyn GameState);
   /// Update game state, NPC behaviors, environment changes, etc.
-  fn update(&self);
+  fn update(&self, state: &mut dyn GameState);
   /// Send updates to players or render the game state in some form.
-  fn render(&self);
+  fn render(&self, state: &mut dyn GameState);
   /// Perform any necessary cleanup before the game loop exits.
-  fn teardown(&self);
+  fn teardown(&self, state: &mut dyn GameState);
 }
 
 #[cfg(test)]
 mod tests {
   use super::*;
 
-  struct TestGameLoop {
-    finished: bool,
+  struct TestGameState {
+    quit_flag: bool,
   }
 
+  impl GameState for TestGameState {
+    fn get_quit_flag(&self) -> bool {
+      self.quit_flag
+    }
+    fn set_quit_flag(&mut self, quit_flag: bool) {
+      self.quit_flag = quit_flag;
+    }
+  }
+
+  struct TestGameLoop {}
+
   impl GameLoop for TestGameLoop {
-    fn setup(&self) {
+    fn setup(&self, _state: &mut dyn GameState) {
       println!("Setting up game loop...");
     }
-    fn is_finished(&self) -> bool {
-      self.finished
-    }
-    fn process_input(&self) {
+    fn process_input(&self, _state: &mut dyn GameState) {
       println!("Processing input...");
     }
-    fn update(&self) {
+    fn update(&self, _state: &mut dyn GameState) {
       println!("Updating game state...");
     }
-    fn render(&self) {
+    fn render(&self, state: &mut dyn GameState) {
+      state.set_quit_flag(true);
       println!("Rendering game state...");
     }
-    fn teardown(&self) {
+    fn teardown(&self, _state: &mut dyn GameState) {
       println!("Tearing down game loop...");
     }
   }
 
   #[test]
   fn test_game_loop() {
-    let game_loop = TestGameLoop { finished: true };
-    game_loop.run();
+    let game_loop = TestGameLoop {};
+    let mut game_state = TestGameState { quit_flag: false };
+    game_loop.run(&mut game_state);
   }
 }
