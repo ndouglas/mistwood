6a2fc19da32a9c8fb57e3d9e8755bb10d5a42f63
Nathan Douglas <github@darkdell.net>
2024-03-24 17:50:36 -0400

    Documentation tweaks.

--- src/input/_traits/handler.rs
+++ src/input/_traits/handler.rs
@@ -3,6 +3,9 @@ use crate::input::_errors::input::InputError;
 use crate::input::input::Input;
 
 /// A trait for input handlers.
+///
+/// An input handler reads input from the user and uses some process to derive
+/// a command from it.
 pub trait Handler {
   /// Read input from the handler.
   fn read(&mut self, input: &Input) -> Result<Box<dyn Command>, InputError>;
